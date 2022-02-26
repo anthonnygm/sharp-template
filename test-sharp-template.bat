c6d0ca8cab04b2ade07076c6fcba9eb82c0d2df4
guilhermeappel <guilhermeaappel@gmail.com>
2022-02-25 23:02:38 -0300

    updated test bat file

--- test-sharp-template.bat
+++ test-sharp-template.bat
@@ -5,20 +5,20 @@ if exist deploy-test rd /q /s deploy-test
 mkdir deploy-test
 
 rem compilar projeto
-pushd Appel.SharpTemplate
+pushd src
+pushd Appel.SharpTemplate.API
 call dotnet restore
 call dotnet build
 call dotnet publish -c Release
-xcopy ".\bin\Release\net5.0\publish\*" "..\deploy-test\*" /y /e
+xcopy ".\bin\Release\net6.0\publish\*" "..\..\deploy-test\*" /y /e
+popd
 popd
 
-rem copiar arquivo appsettings.json
-copy .\Appel.SharpTemplate\appsettings.json deploy-test\appsettings.json /y
-
 rem copiar templates de email
-xcopy ".\Appel.SharpTemplate\EmailTemplates\*" deploy-test\EmailTemplates\ /y /e
+xcopy ".\src\Appel.SharpTemplate.Infrastructure\Application\EmailTemplates\*" deploy-test\EmailTemplates\ /y /e
 
 rem iniciar web api
 pushd deploy-test\
-start call dotnet .\Appel.SharpTemplate.dll
+set ASPNETCORE_ENVIRONMENT=Development
+start call dotnet .\Appel.SharpTemplate.API.dll
 popd

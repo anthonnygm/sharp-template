d5f79aa3505a8f11bcd6768cc83fcbb3b6f0cd83
guilhermeappel <guilhermeaappel@gmail.com>
2022-02-28 22:58:30 -0300

    added use urls from environment

--- src/Appel.SharpTemplate.API/Program.cs
+++ src/Appel.SharpTemplate.API/Program.cs
@@ -2,10 +2,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls($"http://*:{Environment.GetEnvironmentVariable("PORT")}");
 builder.WebHost.UseContentRoot(Directory.GetCurrentDirectory());
 builder.WebHost.UseIISIntegration();
 
